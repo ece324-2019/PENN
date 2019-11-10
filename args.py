12f8b100612bf9dfc8bff6452f86495dd18c6f03
Standard <Standard@Erics-MacBook-Air-3191.local>
2019-11-10 17:42:42 -0500

    Add data preprocessing

--- args.py
+++ args.py
@@ -4,5 +4,16 @@ def get_args():
     # Command Line Arguments
     parser = argparse.ArgumentParser(description='Terminal Arguments for PENN')
     
-    args = parser_args()
+    parser.add_argument('--model', type=str, choices=["baseline", "cnn"], default="cnn")
+    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--lr', type=float, default=0.01)
+    parser.add_argument('--epochs', type=int, default=25)
+    parser.add_argument('--overfit', action="store_true", default=False)
+
+    args = parser.parse_args()
     return args
+
+if __name__ == "__main__":
+
+    args = get_args()
+    print(args)
