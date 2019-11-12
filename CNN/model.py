b476e33cd790be478dfe7dc49d72d6f9a42e88bb
Gabriel Deza <gabriel@Gabriels-MacBook-Pro.local>
2019-11-12 15:47:02 -0500

    Fixed CNN

--- CNN/model.py
+++ CNN/model.py
@@ -1,21 +1,33 @@
 import torch.nn as nn
-import torch.nn.functional as F
 
 class CNN(nn.Module):
     def __init__(self, n_mfcc):
         super(CNN, self).__init__()
-        n_classes = 16 #didnt add up calm and neutral yet
-        kernerl_dim = (4,10)
-        #input is of size (30, 216,1) ie: 2D matrix of 30 MFCC bands by 216 audio length
+        n_classes = 16
+        # We can group up Calm and Neutral to get 14 instead of 16 labels
+        # input is of size (batch_size, 30, 216, 1)
+        # applying 32 kernels of size of (4,10)
         self.conv1 = nn.Sequential(
-                    nn.Conv2d(in_channels=n_classes, out_channels=32, kernel_size=kernel_dim),
-                    nn.BatchNorm2d(n_classes),
+                    nn.Conv2d(in_channels = n_mfcc, out_channels = 32, kernel_size = (4,10)),
+                    nn.BatchNorm2d(4),
                     nn.ReLU(),
-                    nn.MaxPool2d(kernel_dim),
+                    nn.MaxPool2d(4),
                     nn.Dropout(p = 0.2))
+        self.output = nn.Sequential(
+                        nn.Dropout( p =0.2),
+                        nn.BatchNorm2d(4),
+                        nn.ReLU(),
+                        nn.Dropout(p=0.2)
+                        )
+
     def forward(self, x):
         x = self.conv1(x)
         x = self.conv1(x)
         x = self.conv1(x)
         x = self.conv1(x)
+        self.linear =  nn.Linear(x.size[0],64)
+        x = self.linear(x)
+        x = self.output(x)
+        self.end = nn.Sequential(nn.Linear(x.size[0], 16),nn.Softmax())
+        x = self.end(x)
         return x
