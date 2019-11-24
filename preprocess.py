3a20c4da31566c85d9078238f892994a9a843a7c
Eric <epkeilty@hotmail.com>
2019-11-24 16:44:22 -0500

    uncommented stuff

--- preprocess.py
+++ preprocess.py
@@ -11,7 +11,7 @@ ROOT = os.path.dirname(os.path.abspath(__file__))
 
 def normalize():
     print("Normalizing Data")
-    
+
     # normalizing training data
     train_data = pd.read_csv(f"{ROOT}/data/train_data.tsv", sep='\t', index_col=0)
     mean = train_data.mean(axis=0)
@@ -47,18 +47,15 @@ if __name__ == "__main__":
     #RAVDESS.rearrange()
     df = RAVDESS.mfcc_conversion()
 
-    """
     # only augmenting male data to keep dataset balanced
     male_labels = [f"male_{emotion.lower()}" for emotion in RAVDESS.Metadata["emotion"].values()]
     df_males = df.loc[df["label"].isin(male_labels)] #only males
     df_females = df[~df["label"].isin(male_labels)]  #only females
     df_males = RAVDESS.augment(df_males, frac=0.65)
     df = pd.concat([df_males, df_females], ignore_index=True)
-    """
 
     le = RAVDESS.split_data(df, le=le, append=False, equalize=False)
 
-    """
     print("Processing SAVEE dataset")
     SAVEE = SAVEE_Preprocessor(seed=100, n_mfcc=30)
     #SAVEE.rearrange()
@@ -71,7 +68,6 @@ if __name__ == "__main__":
     #TESS.rearrange()
     df = TESS.mfcc_conversion()
     le = TESS.split_data(df, le=le, append=True)
-    """
 
     # Normalize all data (not just the TESS data)
     normalize()
